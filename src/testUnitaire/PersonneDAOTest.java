0cfcb75223e77324368d5b43839fceaf456bd176
Psychadia <kherchouche.remy@gmail.com>
2016-06-13 11:46:55 +0200

    Initialization of unit test package

---
+++ src/testUnitaire/PersonneDAOTest.java
@@ -0,0 +1,48 @@
+package testUnitaire;
+
+import bdd.objetsdao.PersonneDAO;
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+public class PersonneDAOTest extends TestCase {
+
+	private PersonneDAO repo = null;
+
+	public void testFindInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testCreatePersonne() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testUpdatePersonne() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testDeletePersonne() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetByNomPrenom() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testFindByPicture() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		repo = new PersonneDAO();
+	}
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		repo = null;
+	}
+	
+	public static void main(String[] args) {
+        TestRunner.run(PersonneDAOTest.class);
+  }
+
+}
